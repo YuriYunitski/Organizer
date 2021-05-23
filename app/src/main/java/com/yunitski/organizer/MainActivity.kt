af1d2628d275bf1621b326fa9044a45dc0f833a0
YuriYunitski <Lindio@yandex.ru>
2021-05-23 13:55:33 +0300

    2 columns

--- app/src/main/java/com/yunitski/organizer/MainActivity.kt
+++ app/src/main/java/com/yunitski/organizer/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.yunitski.organizer
 
 import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
@@ -15,6 +17,7 @@ import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -27,24 +30,47 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private var toolBar: Toolbar? = null
     private var fab: FloatingActionButton? = null
     private lateinit var adapter: ElementAdapter
+    private lateinit var adapterTwo: ElementAdapter
+    lateinit var sharedPreferences: SharedPreferences
+    var layout: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        toolBar = findViewById(R.id.tool_bar)
-        setSupportActionBar(toolBar)
-        title = "Заметки"
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowTitleEnabled(true)
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val toggle =
-            ActionBarDrawerToggle(this, drawerLayout, toolBar, R.string.open, R.string.close)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-        val navigationView = findViewById<NavigationView>(R.id.nav_view)
-        navigationView.setNavigationItemSelectedListener(this)
-        fab = findViewById(R.id.floatingActionButton2)
-        fab?.setOnClickListener(this)
+        sharedPreferences = getSharedPreferences(Companion.LAYOUT_FILE, Context.MODE_PRIVATE)
+        layout = sharedPreferences.getString(LAYOUT_KEY, ONE_COLUMN_LAYOUT)
+        if (layout == ONE_COLUMN_LAYOUT) {
+            setContentView(R.layout.activity_main)
+            toolBar = findViewById(R.id.tool_bar)
+            setSupportActionBar(toolBar)
+            title = "Заметки"
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayShowTitleEnabled(true)
+            val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+            val toggle =
+                ActionBarDrawerToggle(this, drawerLayout, toolBar, R.string.open, R.string.close)
+            drawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
+            val navigationView = findViewById<NavigationView>(R.id.nav_view)
+            navigationView.setNavigationItemSelectedListener(this)
+            fab = findViewById(R.id.floatingActionButton2)
+            fab?.setOnClickListener(this)
+        } else {
+            setContentView(R.layout.activity_main_two_columns)
+            toolBar = findViewById(R.id.tool_bar_tc)
+            setSupportActionBar(toolBar)
+            title = "Заметки"
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayShowTitleEnabled(true)
+            val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout_two_columns)
+            val toggle =
+                ActionBarDrawerToggle(this, drawerLayout, toolBar, R.string.open, R.string.close)
+            drawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
+            val navigationView = findViewById<NavigationView>(R.id.nav_view_tc)
+            navigationView.setNavigationItemSelectedListener(this)
+            fab = findViewById(R.id.floatingActionButton_tc)
+            fab?.setOnClickListener(this)
+        }
         updateUi()
     }
 
@@ -55,11 +81,19 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         searchView.queryHint = "Текст для поиска"
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
-                adapter.filter.filter(query)
+                if (layout == ONE_COLUMN_LAYOUT){
+                    adapter.filter.filter(query)
+                } else{
+                    adapterTwo.filter.filter(query)
+                }
                 return false
             }
             override fun onQueryTextChange(newText: String): Boolean {
-                adapter.filter.filter(newText)
+                if (layout == ONE_COLUMN_LAYOUT){
+                    adapter.filter.filter(newText)
+                } else{
+                    adapterTwo.filter.filter(newText)
+                }
                 return false
             }
 
@@ -69,7 +103,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onContextItemSelected(item: MenuItem): Boolean {
-        val position: Int = adapter.getPosition()
+        var position = 0
+        if (layout == ONE_COLUMN_LAYOUT){
+            position = adapter.getPosition()
+        } else {
+            position = adapterTwo.getPosition()
+        }
         val myDBHandler = MyDBHandler(this, "notesDB.db", null, 1)
         val db: SQLiteDatabase = myDBHandler.writableDatabase
         val myDBHandlerArchive = MyDBHandlerArchive(this, "notesDBarchive.db", null, 1)
@@ -98,33 +137,76 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         db.delete(MyDBHandler.TABLE_NOTES, MyDBHandler.COLUMN_ID + "=?", arrayOf(position.toString()))
         aDb.close()
         db.close()
+
+
         updateUi()
         return super.onContextItemSelected(item)
     }
 
     override fun onResume() {
         super.onResume()
+        sharedPreferences = getSharedPreferences(Companion.LAYOUT_FILE, Context.MODE_PRIVATE)
+        layout = sharedPreferences.getString(LAYOUT_KEY, ONE_COLUMN_LAYOUT)
+        if (layout == ONE_COLUMN_LAYOUT) {
+            setContentView(R.layout.activity_main)
+            toolBar = findViewById(R.id.tool_bar)
+            setSupportActionBar(toolBar)
+            title = "Заметки"
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayShowTitleEnabled(true)
+            val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+            val toggle =
+                ActionBarDrawerToggle(this, drawerLayout, toolBar, R.string.open, R.string.close)
+            drawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
+            val navigationView = findViewById<NavigationView>(R.id.nav_view)
+            navigationView.setNavigationItemSelectedListener(this)
+            fab = findViewById(R.id.floatingActionButton2)
+            fab?.setOnClickListener(this)
+        } else {
+            setContentView(R.layout.activity_main_two_columns)
+            toolBar = findViewById(R.id.tool_bar_tc)
+            setSupportActionBar(toolBar)
+            title = "Заметки"
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayShowTitleEnabled(true)
+            val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout_two_columns)
+            val toggle =
+                ActionBarDrawerToggle(this, drawerLayout, toolBar, R.string.open, R.string.close)
+            drawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
+            val navigationView = findViewById<NavigationView>(R.id.nav_view_tc)
+            navigationView.setNavigationItemSelectedListener(this)
+            fab = findViewById(R.id.floatingActionButton_tc)
+            fab?.setOnClickListener(this)
+        }
         updateUi()
     }
 
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         val id: Int = item.itemId
-        if (id == R.id.remind){
-            startActivity(Intent(this, RemindActivity::class.java))
-        } else if(id == R.id.settings){
+//        if (id == R.id.remind){
+//            startActivity(Intent(this, RemindActivity::class.java))
+//        } else
+            if(id == R.id.settings){
             startActivity(Intent(this, Settings::class.java))
         } else if(id == R.id.archive){
             startActivity(Intent(this, Archive::class.java))
         }
-        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
-        drawerLayout.closeDrawer(GravityCompat.START)
+        if(layout == ONE_COLUMN_LAYOUT) {
+            val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            val drawerLayout = findViewById<View>(R.id.drawer_layout_two_columns) as DrawerLayout
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
         return true
     }
 
     override fun onClick(v: View?) {
         when(v?.id){
-            R.id.floatingActionButton2 ->{
+            R.id.floatingActionButton2, R.id.floatingActionButton_tc ->{
                 newNote()
             }
         }
@@ -142,12 +224,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     private fun updateUi() {
-        val recyclerView: RecyclerView = findViewById(R.id.recycler_list)
-        recyclerView.layoutManager = LinearLayoutManager(this)
         val messageList: MutableList<String> = mutableListOf()
         val titleList: MutableList<String> = mutableListOf()
-        val elements: MutableList<Element> = mutableListOf()
         val idList: MutableList<String> = mutableListOf()
+        val elements: MutableList<Element> = mutableListOf()
         val myDBHandler = MyDBHandler(this, "notesDB.db", null, 1)
         val db: SQLiteDatabase = myDBHandler.readableDatabase
         val cursor: Cursor = db.rawQuery("SELECT * FROM " + MyDBHandler.TABLE_NOTES + ";", null)
@@ -178,26 +258,57 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
         db.close()
         cursor.close()
-        adapter = ElementAdapter(this, elements, this)
-        recyclerView.adapter = adapter
-        recyclerView.addOnItemTouchListener(
-            ElementTouchListener(
-                applicationContext,
-                object : ElementTouchListener.OnItemClickListener {
-                    override fun onItemClick(view: View?, position: Int) {
-                        Toast.makeText(
-                            applicationContext,
-                            "You clicked " + idList[position],
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                })
-        )
+        val recyclerView: RecyclerView
+        val recyclerViewTC: RecyclerView
+        if (layout == ONE_COLUMN_LAYOUT){
+            adapter = ElementAdapter(this, elements, this)
+            recyclerView= findViewById(R.id.recycler_list)
+            recyclerView.layoutManager = LinearLayoutManager(this)
+            recyclerView.adapter = adapter
+            recyclerView.addOnItemTouchListener(
+                ElementTouchListener(
+                    applicationContext,
+                    object : ElementTouchListener.OnItemClickListener {
+                        override fun onItemClick(view: View?, position: Int) {
+                            Toast.makeText(
+                                applicationContext,
+                                "You clicked " + idList[position],
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    })
+            )
+        } else if (layout == TWO_COLUMN_LAYOUT){
+            adapterTwo = ElementAdapter(this, elements, this)
+            recyclerViewTC = findViewById(R.id.recycler_tc)
+            recyclerViewTC.layoutManager = GridLayoutManager(this, 2)
+            recyclerViewTC.adapter = adapterTwo
+            recyclerViewTC.addOnItemTouchListener(
+                ElementTouchListener(
+                    applicationContext,
+                    object : ElementTouchListener.OnItemClickListener {
+                        override fun onItemClick(view: View?, position: Int) {
+                            Toast.makeText(
+                                applicationContext,
+                                "You clicked " + idList[position],
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    })
+            )
+        }
     }
 
     override fun onElementSelected(elts: Element?) {
         val intent = Intent(this, EditNote::class.java)
         intent.putExtra("id", elts?.id)
         startActivity(intent)
+    }
+
+    companion object {
+        const val LAYOUT_FILE: String = "layoutFile"
+        const val ONE_COLUMN_LAYOUT = "oneColumnLayout"
+        const val TWO_COLUMN_LAYOUT = "twoColumnLayout"
+        const val LAYOUT_KEY = "1664"
     }
 }
--- app/src/main/java/com/yunitski/organizer/Settings.kt
+++ app/src/main/java/com/yunitski/organizer/Settings.kt
@@ -1,11 +1,21 @@
 package com.yunitski.organizer
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 
-class Settings : AppCompatActivity() {
+class Settings : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var listBtn: TextView
+    lateinit var columnBtn: TextView
+    lateinit var sharedPreferences: SharedPreferences
+    var layout: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -14,6 +24,19 @@ class Settings : AppCompatActivity() {
         title = "Настройки"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(true)
+        listBtn = findViewById(R.id.list_set)
+        columnBtn = findViewById(R.id.column_set)
+        listBtn.setOnClickListener(this)
+        columnBtn.setOnClickListener(this)
+        sharedPreferences = getSharedPreferences(MainActivity.LAYOUT_FILE, Context.MODE_PRIVATE)
+        layout = sharedPreferences.getString(MainActivity.LAYOUT_KEY, MainActivity.ONE_COLUMN_LAYOUT)
+        if (layout == MainActivity.ONE_COLUMN_LAYOUT){
+            listBtn.setBackground(resources.getDrawable(R.drawable.back_gray))
+            columnBtn.setBackground(resources.getDrawable(R.drawable.back_white))
+        } else {
+            columnBtn.setBackground(resources.getDrawable(R.drawable.back_gray))
+            listBtn.setBackground(resources.getDrawable(R.drawable.back_white))
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -23,5 +46,23 @@ class Settings : AppCompatActivity() {
             }
         }
         return true
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.list_set){
+            listBtn.setBackground(resources.getDrawable(R.drawable.back_gray))
+            columnBtn.setBackground(resources.getDrawable(R.drawable.back_white))
+            sharedPreferences = getSharedPreferences(MainActivity.LAYOUT_FILE, Context.MODE_PRIVATE)
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putString(MainActivity.LAYOUT_KEY, MainActivity.ONE_COLUMN_LAYOUT)
+            editor.apply()
+        } else if (v?.id == R.id.column_set){
+            columnBtn.setBackground(resources.getDrawable(R.drawable.back_gray))
+            listBtn.setBackground(resources.getDrawable(R.drawable.back_white))
+            sharedPreferences = getSharedPreferences(MainActivity.LAYOUT_FILE, Context.MODE_PRIVATE)
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putString(MainActivity.LAYOUT_KEY, MainActivity.TWO_COLUMN_LAYOUT)
+            editor.apply()
+        }
     }
 }
